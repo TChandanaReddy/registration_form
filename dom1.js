7a489870064d0fecdef179bc286d06de2208f12a
TChandanaReddy <tchandana.reddy1997@gmail.com>
2023-04-15 12:53:50 +0530

    getelemntbyClassName

---
+++ dom1.js
@@ -0,0 +1,11 @@
+// GETELEMENTBYCLASSNAME //
+var items=document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[1]);
+// items[1].textContent='Hello2';
+//items[1].style.fontWeight='bold';
+items[2].style.backgroundColor='green';
+
+for(var i=0; i<items.length;i++){
+   items[i].style.fontWeight='bold';
+}
