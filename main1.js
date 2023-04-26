30bdab8ccd8f6c11c775e983ebafd6f6dfbdf589
TChandanaReddy <tchandana.reddy1997@gmail.com>
2023-04-26 13:44:46 +0530

    store the users in right format

--- main1.js
+++ main1.js
@@ -7,6 +7,15 @@ btn.addEventListener('click', (e) => {
     const name=document.getElementById('name').value;
     const email=document.getElementById('email').value;
 
+    let myObj={
+        name:name,
+        email:email
+    };
+    let myObj_serialized=JSON.stringify(myObj);
+    localStorage.setItem('myObj',myObj_serialized);
+    let myObj_deserialized=JSON.parse(localStorage.getItem('myObj'));
+    console.log(myObj_deserialized);
+
     localStorage.setItem('name',name);
     localStorage.setItem('email',email);
 });
@@ -24,4 +33,5 @@ btn.addEventListener('mouseout', (e) => {
     document.querySelector('body').classList.add('bg-dark');
     console.log('mouseout');
 });
+
 
