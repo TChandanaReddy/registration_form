ff776dc20512667e4d04065584e0f84baaf5c4d0
TChandanaReddy <tchandana.reddy1997@gmail.com>
2023-05-04 13:33:18 +0530

    scaling the app to more users

--- main1.js
+++ main1.js
@@ -1,37 +1,52 @@
-const btn = document.querySelector('.btn');
+// const btn = document.querySelector('.btn');
 
-btn.addEventListener('click', (e) => {
-    e.preventDefault();
-    document.querySelector('#my-form').style.background ='#ccc'
-    document.querySelector('body').classList.add('bg-dark');
-    const name=document.getElementById('name').value;
-    const email=document.getElementById('email').value;
+// btn.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     document.querySelector('#my-form').style.background ='#ccc'
+//     document.querySelector('body').classList.add('bg-dark');
+//     const name=document.getElementById('name').value;
+//     const email=document.getElementById('email').value;
 
-    let myObj={
-        name:name,
-        email:email
+//     let myObj={
+//         name:name,
+//         email:email
+//     };
+//     let myObj_serialized=JSON.stringify(myObj);
+//     localStorage.setItem('myObj',myObj_serialized);
+//     let myObj_deserialized=JSON.parse(localStorage.getItem('myObj'));
+//     console.log(myObj_deserialized);
+
+//     localStorage.setItem('name',name);
+//     localStorage.setItem('email',email);
+// });
+
+// btn.addEventListener('mouseover', (e) => {
+//     e.preventDefault();
+//     document.querySelector('#my-form').style.background ='#ccc'
+//     document.querySelector('body').classList.add('bg-dark');
+//     console.log('mouseover');
+// });
+
+// btn.addEventListener('mouseout', (e) => {
+//     e.preventDefault();
+//     document.querySelector('#my-form').style.background ='#ccc'
+//     document.querySelector('body').classList.add('bg-dark');
+//     console.log('mouseout');
+// });
+
+function saveToLocalStorage(event){
+    event.preventDefault();
+    const name=event.target.username.value;
+    const email=event.target.emailid.value;
+
+    const obj={
+        name,
+        email
     };
-    let myObj_serialized=JSON.stringify(myObj);
-    localStorage.setItem('myObj',myObj_serialized);
-    let myObj_deserialized=JSON.parse(localStorage.getItem('myObj'));
-    console.log(myObj_deserialized);
-
-    localStorage.setItem('name',name);
-    localStorage.setItem('email',email);
-});
-
-btn.addEventListener('mouseover', (e) => {
-    e.preventDefault();
-    document.querySelector('#my-form').style.background ='#ccc'
-    document.querySelector('body').classList.add('bg-dark');
-    console.log('mouseover');
-});
-
-btn.addEventListener('mouseout', (e) => {
-    e.preventDefault();
-    document.querySelector('#my-form').style.background ='#ccc'
-    document.querySelector('body').classList.add('bg-dark');
-    console.log('mouseout');
-});
-
-
+    localStorage.setItem('obj',JSON.stringify(obj));
+    showUserOnScreen(obj);
+}
+function showUserOnScreen(obj){
+    const parentElem=document.getElementById('users');
+    parentElem.innerHTML=parentElem.innerHTML + `<li>${obj.name} - ${obj.email}</li>`
+}
